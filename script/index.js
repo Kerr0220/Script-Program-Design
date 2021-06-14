830e8d168369d4c831fef13ffff7bc8abdafe01d
Kerr <kerr99801@gmail.com>
2021-06-14 16:46:58 +0800

    index page

---
+++ script/index.js
@@ -0,0 +1,9 @@
+class Task {
+    constructor(title, content, date) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.isFinished = false;
+        this.priority = 0;
+    }
+}
